b3a8346f945d09d91736c4c6a38b525082ba5c76
Sergey Chernyshov <thesaren117@yandex.ru>
2022-03-21 22:35:05 +0300

    sql change

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_stocks_products',
 
     }
